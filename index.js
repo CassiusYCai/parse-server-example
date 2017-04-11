fc73d107102becdab0edb015eb2d581059617502
CassiusYCai <cassius.y.cai@gmail.com>
2017-04-10 23:10:37 -0400

    push notifications added to index.js

--- index.js
+++ index.js
@@ -19,20 +19,20 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://citizen-congress.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  } // ,
-  // push: {
-  //   android: {
-  //     senderId: '',
-  //     apiKey: ''
-  //   },
-  //   ios: {
-  //     pfx: 'certs/mycert.p12'.
-  //     cert: '',
-  //     bundleId: 'com.CitizenYbTAyg7Nj6jBrgWK4TpXiuFEjhN26gHghht3d7vb2KqUrii8',
-  //     key: '',
-  //     production: false
-  //   }
-  // }
+  },
+  push: {
+    android: {
+      senderId: '',
+      apiKey: ''
+    },
+    ios: {
+      pfx: 'certs/mycert.p12',
+      cert: '',
+      bundleId: 'com.CitizenYbTAyg7Nj6jBrgWK4TpXiuFEjhN26gHghht3d7vb2KqUrii8',
+      key: '',
+      production: false
+    }
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
